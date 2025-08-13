63b928bffd222d0e2f50b0289f334b129c86837e
ischen99 <ischen_99@web.de>
2025-08-13 15:18:42 +0200

    Update page.tsx

--- src/app/(sidebar)/create/page.tsx
+++ src/app/(sidebar)/create/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <h1 className="text-2xl font-bold">Features</h1>;
+export default function CreatePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Create</h1>
+      <p>Hier kannst du ein neues Video oder Projekt erstellen.</p>
+    </div>
+  )
 }
